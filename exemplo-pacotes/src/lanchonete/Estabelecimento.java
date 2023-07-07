b6afd33c0d2fe4f07b861fb384dacd9a116ddc1d
axemay <maiara.accacio@gmail.com>
2023-07-07 07:41:24 -0300

    update

--- exemplo-pacotes/src/lanchonete/Estabelecimento.java
+++ exemplo-pacotes/src/lanchonete/Estabelecimento.java
@@ -1,35 +1,25 @@
 package lanchonete;
 
+import lanchonete.area.atendimento.cozinha.Almoxarife;
+import lanchonete.area.atendimento.Atendente;
+import lanchonete.area.atendimento.cozinha.Cozinheiro;
+import lanchonete.area.cliente.Cliente;
+
 public class Estabelecimento {
     public static void main(String[] args) {
         Cozinheiro cozinheiro = new Cozinheiro();
-        //ações que não precisam estarem disponíveis para toda a aplicação
-        cozinheiro.lavarIngredientes();
-        cozinheiro.baterVitaminaLiquidificador();
-        cozinheiro.selecionarIngredientesVitamina();
-        cozinheiro.prepararLanche();
-        cozinheiro.prepararVitamina();
-        cozinheiro.prepararVitamina();
 
         //ações que estabelecimento precisa ter ciência
         cozinheiro.adicionarSucoNoBalcao();;
         cozinheiro.adicionarLancheNoBalcao();
         cozinheiro.adicionarComboNoBalcao();
 
-        Almoxarife almoxarife = new Almoxarife();
-        //ações que não precisam estarem disponíveis para toda a aplicação
-        almoxarife.controlarEntrada();
-        almoxarife.controlarSaida();
-        //ação que somente o seu pacote cozinha precisa conhecer (default)
-        almoxarife.entregarIngredientes();
-        almoxarife.trocarGas();
 
         Atendente atendente = new Atendente();
         atendente.pegarLancheCozinha();
         atendente.receberPagamento();
         atendente.servindoMesa();
-        //ação que somente o seu pacote cozinha precisa conhecer (default)
-        atendente.trocarGas();
+
 
         Cliente cliente = new Cliente();
         cliente.escolherLanche();
@@ -43,9 +33,6 @@ public class Estabelecimento {
         //esta ação é muito sigilosa, qua tal ser privada ?
         cliente.consultarSaldoAplicativo();
 
-        //já pensou os clientes ouvindo que o gás acabou ?
-        cozinheiro.pedirParaTrocarGas(atendente);
-        cozinheiro.pedirParaTrocarGas(almoxarife);
 
     }
 }
